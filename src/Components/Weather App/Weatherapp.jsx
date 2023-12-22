84265a4c5b984098c170d457fced89a2df73fd46
fatima678 <119948111+fatima678@users.noreply.github.com>
2023-12-23 00:23:25 +0500

    style the weather app with css

--- src/Components/Weather App/Weatherapp.jsx
+++ src/Components/Weather App/Weatherapp.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import './Weatherapp.css'
 import search_icon from "../Assets/search.png";
 
+
 const Weatherapp = () => {
   return (
-    <div>
+    <div className='container'>
+        <div className='top-bar'>
+            <input type='text' className='cityInput' placeholder='Search'></input>
+            <div className='search-icon'>
+                <img src={search_icon} alt=''></img>
+            </div>
+        </div>
       
     </div>
   )
