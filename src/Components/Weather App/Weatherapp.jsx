27af40e50b02b260db37a44b8e3c84f49b248b6a
fatima678 <119948111+fatima678@users.noreply.github.com>
2023-12-22 23:38:32 +0500

    Weather App using React js

---
+++ src/Components/Weather App/Weatherapp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Weatherapp.css'
+import search_icon from "../Assets/search.png";
+
+const Weatherapp = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Weatherapp
